6448f4cb80ad8a3357dc625d0496c472aae47483
EranTayeb <156083739+EranTayeb@users.noreply.github.com>
2024-06-17 13:12:45 +0300

    project5

---
+++ freertosexe18/src/freertosexe18.c
@@ -0,0 +1,101 @@
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "timers.h"
+#include "semphr.h"
+#include "xgpio.h"
+#include "xparameters.h"
+#include "xil_printf.h"
+#include "queue.h"
+#include "xuartps.h"
+#include <stdio.h>
+#include <string.h>
+#include "dht.h"
+#include "semphr.h"
+#include "event_groups.h"
+
+#define GPIO_DEVICE_ID XPAR_GPIO_0_DEVICE_ID
+#define LED1 (0x01 << 20)
+#define LED2 (0x01 << 21)
+#define dhtPin (0x01 << 27)
+
+#define event_read (1 << 0)
+#define event_print (1 << 1)
+
+void TaskDHT();
+void TaskDelay();
+void TaskPrint();
+
+SemaphoreHandle_t semDelay;
+SemaphoreHandle_t semPrint;
+Dht *sensor ;
+XGpio Gpio;
+#define LED_CHANNEL 1
+
+int  temperature ;
+int  humidity ;
+EventGroupHandle_t  eventgroup ;
+
+int main(){
+
+
+	//Initialize GPIO
+	int Status = XGpio_Initialize(&Gpio, XPAR_GPIO_0_DEVICE_ID);
+	if (Status != XST_SUCCESS) {
+			xil_printf("Gpio Initialization Failed\r\n");
+			return XST_FAILURE;
+		}
+
+	sensor = Dht_init(&Gpio,dhtPin);
+
+    xTaskCreate(TaskDHT, "TaskDHT", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);
+    xTaskCreate(TaskDelay, "TaskDelay", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(TaskPrint, "TaskPrint", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+
+    eventgroup = xEventGroupCreate();
+
+
+
+    vTaskStartScheduler(); // Start the scheduler
+
+    while(1); // Should never reach here
+    return 0;
+
+}
+
+void TaskDHT() {
+    while(1) {
+
+    	xEventGroupWaitBits(eventgroup,
+    			event_read,
+				pdTRUE,
+				pdFALSE,
+				portMAX_DELAY );
+    	Dht_takeData(sensor);
+    	Dht_getResult(sensor, &temperature, &humidity);
+    	xEventGroupSetBits(eventgroup,
+    			event_print );
+    }
+}
+
+void TaskPrint() {
+	while(1){
+    	xEventGroupWaitBits(eventgroup,
+    			event_print,
+				pdTRUE,
+				pdFALSE,
+				portMAX_DELAY );
+		xil_printf("temperature = %d  , humidity = %d \r\n" , temperature ,humidity);
+	}
+
+}
+
+void TaskDelay() {
+    const TickType_t xDelay = pdMS_TO_TICKS(2000); // 2 seconds delay
+    while(1) {
+        vTaskDelay(xDelay); // Delay for 2 seconds
+    	xEventGroupSetBits(eventgroup,
+    			event_read );
+
+    }
+}
